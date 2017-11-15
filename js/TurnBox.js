be4033c00768d9546762eb211c8f9f447123416e
unknown <nicolas.cusumano0@gmail.com>
2017-11-15 16:13:22 +0100

    turn box with arrows

---
+++ js/TurnBox.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", init, false);
+
+var degree = 0;
+var cube;
+const Z = "translateZ(50vw) ";
+
+function init() {
+  cube = document.getElementsByClassName("cube")[0];
+  window.addEventListener("keydown", turn, false);
+}
+
+function turn(e) {
+  if(e.key == "ArrowLeft") {
+    turnBox(-90);
+  } else if (e.key == "ArrowRight") {
+    turnBox(90);
+  }
+}
+
+function turnBox(param) {
+  degree += param;
+  if(cube !== undefined) {
+    cube.style.transform = Z + "rotateY("+degree+"deg)";
+  }
+  console.log(degree);
+}
