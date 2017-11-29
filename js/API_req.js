cae56429013a9db952aab2012e8561d58eb4c8df
Bastien Germain <bastiengermain1708@hotmail.fr>
2017-11-29 14:38:32 +0100

    requete lyrics + turnbox

--- js/API_req.js
+++ js/API_req.js
@@ -1,9 +1,11 @@
 var url ="http://api.musixmatch.com/ws/1.1/track.search?apikey=0b87e68516c5e3ca3841fb358f30889c&page=1&page_size="; 
-var nb_result = 5;
+var nb_result = 3;
 
 $(document).ready(function(){
 
-    $('#submit').on('click', function(){
+    $('#lyrics_form').submit(function(e) {
+
+        e.preventDefault();
 
         var lyrics = $("#search").val();
 
@@ -15,12 +17,16 @@ $(document).ready(function(){
             success: function (data) {
                 var obj = JSON.parse(data);
 
+                $("#result").empty();
+
+
                 for(i = 0; i < nb_result; i++) {
-                    console.log("Result " + i + ":");
-                    console.log("Track : " + obj.message.body.track_list[i].track.track_name);
-                    console.log("Artist : " + obj.message.body.track_list[i].track.artist_name);
-                    console.log("Album : " + obj.message.body.track_list[i].track.album_name);
+                    $("#result").append("<p>Track : " + obj.message.body.track_list[i].track.track_name + "</p>");
+                    $("#result").append("<p>Artist : " + obj.message.body.track_list[i].track.artist_name + "</p>");
+                    $("#result").append("<p>Album : " + obj.message.body.track_list[i].track.album_name + "</p>");
+                    $("#result").append("<button class='listen'>Listen !</button>");
                 }
+                turnBox(90);
                 
             },
 
@@ -30,5 +36,9 @@ $(document).ready(function(){
             }
 
         });
+    });
+
+    $('body').on('click', '.listen', function(){
+        turnBox(90);
     });
 });
