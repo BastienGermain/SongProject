5555d07eb3506704e661a1d72943fd7fcedcbabb
Bastien Germain <bastiengermain1708@hotmail.fr>
2017-12-06 18:55:26 +0100

    ajout requete cinema

--- js/API_GMaps.js
+++ js/API_GMaps.js
@@ -1,18 +1,8 @@
-const APICODE = "AIzaSyDNaCN1rRf2iAAhFmgT0tQ2LIzNf6UM5ws";
-
 let uluru = {lat: 0.0, lng: 0.0};
 
 function findAdresse(adresse) {
-  uluru.lat = adresse.geometry.coordinates[0];
-  uluru.lng = adresse.geometry.coordinates[1];
-
-  var src="https://maps.googleapis.com/maps/api/js?key="+APICODE+"&callback="+initMap.name;
-
-  var script = $("<script async defer></script");
-  script.src = src;
-
-  $('#result').append(script);
-
+  uluru.lat = adresse[0];
+  uluru.lng = adresse[1];
 }
 
 function initMap() {
--- js/API_reqAddress.js
+++ js/API_reqAddress.js
@@ -3,14 +3,16 @@ var limit = 5;
 
 $(document).ready(function(){
 
+    var address;
+
     function selectSuggestion(nbSuggestion, inputValue, key) {
             var inputDefault = inputValue;
             console.log(nbSuggestion, inputValue, key);
     }
 
-    /* Corrige le retour du curseur */   
+    /* Corrige le retour du curseur */
     $("#search").keydown(function(e) {
-        if (e.keyCode === 38 || e.keyCode === 40) 
+        if (e.keyCode === 38 || e.keyCode === 40)
         return false;
     });
 
@@ -18,7 +20,7 @@ $(document).ready(function(){
 
         var pos = this.selectionStart;
 
-        var address = $("#search").val();
+        address = $("#search").val();
 
         var req = url + limit + "&q=" + address;
         $.ajax({
@@ -42,11 +44,33 @@ $(document).ready(function(){
 
         });
 
-        this.selectionStart = pos; this.selectionEnd = pos;
     });
 
-    $('body').on('click', '.listen', function(){
-        console.log($(this).data("track"));
+    var url_cinema = "https://data.culturecommunication.gouv.fr/api/records/1.0/search/?dataset=etablissements-cinematographiques&q=";
+
+    $('#lyrics__form').submit(function(e) {
+
+        e.preventDefault();
+        console.log(address);
+
+         $.ajax({
+            url: url_cinema + address + "&facet=region_administrative",
+            success: function(data) {
+                var length = data.records.length;
+                for(i = 0; i < length; i++) {
+                    console.log(data.records[i].fields.nom);
+                    console.log(data.records[i].fields.corrdonnees_finales[0]); // lat 
+                    console.log(data.records[i].fields.corrdonnees_finales[1]); // long 
+
+                    findAdresse(data.records[i].fields.corrdonnees_finales);
+                }               
+            }, 
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR, textStatus, errorThrown)
+            }
+        });
+
+
         turnBox(90);
     });
 });
