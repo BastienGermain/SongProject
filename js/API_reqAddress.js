d99bfc235a26610e60d13149a52210ff8e56ec6c
Bastien Germain <bastiengermain1708@hotmail.fr>
2017-12-13 16:58:05 +0100

    test map style + comment

--- js/API_reqAddress.js
+++ js/API_reqAddress.js
@@ -1,28 +1,36 @@
-var url ="https://api-adresse.data.gouv.fr/search/?limit=";
+/* Variable API adresse */
+
+var url_adresse ="https://api-adresse.data.gouv.fr/search/?limit=";
 var nbResult = 5;
+
+/* Variable globales */
+
 var oldAdress = "";
 var address, nbSuggestion, selected;
 
 $(document).ready(function(){
 
     function selectSuggestion(key) {
-            if (key == 40) {
-                selected = (selected == nbSuggestion) ? 0 : selected+1;
-            } else if (key == 38) {
-                selected = (selected == 0) ? nbSuggestion : selected-1;
-            }
-            var index = selected+1;
-            var selectedValue = $('.search__singleResult:nth-child('+index+')');
-            if (selected == nbSuggestion) {
-                $("#search").val( address );
-            } else {
-                $("#search").val( selectedValue.text() );
-            }
-            $('.search__singleResult.selected').removeClass('selected');
-            selectedValue.addClass('selected');
+        if (key == 40) { /* down arrow */
+            selected = (selected == nbSuggestion) ? 0 : selected + 1;
+        } else if (key == 38) { /* up arrow */
+            selected = (selected == 0) ? nbSuggestion : selected - 1;
+        }
+
+        var index = selected + 1;
+        var selectedValue = $('.search__singleResult:nth-child(' + index + ')');
+
+        if (selected == nbSuggestion) {
+            $("#search").val(address);
+        } else {
+            $("#search").val(selectedValue.text());
+        }
+
+        $('.search__singleResult.selected').removeClass('selected');
+        selectedValue.addClass('selected');
     }
 
-    /* Corrige le retour du curseur */
+    /* Corrige le bug du retour du curseur au début de l'input */
     $("#search").keydown(function(e) {
         if (e.which == 38 || e.which == 40) 
         return false;
@@ -31,12 +39,15 @@ $(document).ready(function(){
     $("#search").keyup(function(e) {
         if (e.which !== 38 && e.which !== 40) {
             address = $("#search").val();
+
             if (address !== oldAdress) {
                 oldAdress = address;
-                var req = url + nbResult + "&q=" + address;
+                var req = url_adresse + nbResult + "&q=" + address;
+
                 $.ajax({
                     url: req,
                     success: function(data) {
+                        
                         $(".search__results").empty();
                         var suggestion = data['features'];
                         nbSuggestion = suggestion.length;
@@ -58,6 +69,10 @@ $(document).ready(function(){
         }
     });
 
+    /* 
+    *   Requete API Ministère Culture (liste cinemas) 
+    */
+
     var url_cinema = "https://data.culturecommunication.gouv.fr/api/records/1.0/search/?dataset=etablissements-cinematographiques&q=";
 
     function lanceRequete(valeur) {
@@ -69,8 +84,8 @@ $(document).ready(function(){
                 var length = data.records.length;
                 for(i = 0; i < length; i++) {
                     console.log(data.records[i].fields.nom);
-                    console.log(data.records[i].fields.corrdonnees_finales[0]); // lat 
-                    console.log(data.records[i].fields.corrdonnees_finales[1]); // long 
+                    console.log(data.records[i].fields.corrdonnees_finales[0]); // latitude 
+                    console.log(data.records[i].fields.corrdonnees_finales[1]); // longitude 
 
                     findAdresse(data.records[i].fields.corrdonnees_finales);
                 }               
@@ -79,15 +94,16 @@ $(document).ready(function(){
                 console.log(jqXHR, textStatus, errorThrown)
             }
         });
-
-
         turnBox(90);
     }
+
 
     $('#lyrics__form').submit(function(e) {
         e.preventDefault();
         lanceRequete($("#search").val());
     });
+
+    /* Lance la recherche au click sur les suggestions */
 
     $('body').on('click', '.search__singleResult',function(e) {
         e.preventDefault();
