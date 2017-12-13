6139e227ac870db4398077af59f032c48adced92
qsedmi <quentinsedmi@gmail.com>
2017-12-13 16:03:13 +0100

    Maquette premiere page

--- js/API_reqAddress.js
+++ js/API_reqAddress.js
@@ -1,48 +1,61 @@
 var url ="https://api-adresse.data.gouv.fr/search/?limit=";
-var limit = 5;
-
+var nbResult = 5;
+var oldAdress = "";
+var address, nbSuggestion, selected;
 $(document).ready(function(){
 
-    function selectSuggestion(nbSuggestion, inputValue, key) {
-            var inputDefault = inputValue;
-            console.log(nbSuggestion, inputValue, key);
+    function selectSuggestion(key) {
+            if (key == 40) {
+                selected = (selected == nbSuggestion) ? 0 : selected+1;
+            } else if (key == 38) {
+                selected = (selected == 0) ? nbSuggestion : selected-1;
+            }
+            var index = selected+1;
+            var selectedValue = $('.search__singleResult:nth-child('+index+')');
+            if (selected == nbSuggestion) {
+                $("#search").val( address );
+            } else {
+                $("#search").val( selectedValue.text() );
+            }
+            $('.search__singleResult.selected').removeClass('selected');
+            selectedValue.addClass('selected');  
     }
 
     /* Corrige le retour du curseur */   
     $("#search").keydown(function(e) {
-        if (e.keyCode === 38 || e.keyCode === 40) 
+        if (e.which == 38 || e.which == 40) 
         return false;
     });
 
     $("#search").keyup(function(e) {
-
-        var pos = this.selectionStart;
-
-        var address = $("#search").val();
-
-        var req = url + limit + "&q=" + address;
-        $.ajax({
-            url: req,
-            success: function(data) {
-                $(".search__results").empty();
-                var suggestion = data['features'];
-                if (suggestion.length !== 0) {
-                    for (var i = 0; i < suggestion.length; i++) {
-                        $(".search__results").append('<div class="search__singleResult" id="' + suggestion[i]['properties']['id'] + '">' + suggestion[i]['properties']['label'] + '</div>');
+        if (e.which !== 38 && e.which !== 40) {
+            address = $("#search").val();
+            if (address !== oldAdress) {
+                oldAdress = address;
+                var req = url + nbResult + "&q=" + address;
+                $.ajax({
+                    url: req,
+                    success: function(data) {
+                        $(".search__results").empty();
+                        var suggestion = data['features'];
+                        nbSuggestion = suggestion.length;
+                        if (nbSuggestion !== 0) {
+                            for (var i = 0; i < nbSuggestion; i++) {
+                                $(".search__results").append('<div class="search__singleResult" id="' + suggestion[i]['properties']['id'] + '">' + suggestion[i]['properties']['label'] + '</div>');
+                            }  
+                        }
+                        selected = nbSuggestion;
+                        
+                    }, 
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log(jqXHR, textStatus, errorThrown)
                     }
-                    if (e.which == 38 || e.which == 40) {
-                        selectSuggestion(suggestion.length, address, e.which);
-                    }
-                }
-                
-            }, 
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log(jqXHR, textStatus, errorThrown)
+
+                });
             }
-
-        });
-
-        this.selectionStart = pos; this.selectionEnd = pos;
+        } else {
+            selectSuggestion(e.which);
+        }
     });
 
     $('body').on('click', '.listen', function(){
